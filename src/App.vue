43fbf47910bce2163017531907764ffa001c6a54
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-04-01 17:18:29 -0400

    4-1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-    <div id="app">
+  <div id="app">
+    <!-- Navigation bar -->
+    <nav class="navbar">
+      <router-link to="/" class="navbar-brand">MyApp</router-link>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/signup" class="nav-link">Sign Up</router-link>
+        </li>
+      </ul>
+    </nav>
 
-      <nav class="navbar">
-
-        <router-link to="/" class="navbar-brand">MyApp</router-link>
-  
-
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/signup" class="nav-link">Sign Up</router-link>
-          </li>
-
-        </ul>
-      </nav>
-  
-
-      <main>
-        <router-view></router-view>
-      </main>
-    </div>
+    <!-- Main content -->
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
-  
-<style>
 
+<style>
+  /* General styling */
   .navbar {
     display: flex;
     justify-content: space-between;
@@ -30,26 +26,26 @@
     padding: 1rem;
     background-color: #333;
   }
-  
-  .navbar-brand, .nav-link {
+
+  .navbar-brand,
+  .nav-link {
     color: #fff;
     text-decoration: none;
   }
-  
+
   .navbar-nav {
     list-style: none;
     display: flex;
     margin: 0;
     padding: 0;
   }
-  
+
   .nav-item {
     margin-left: 1rem;
   }
-  
 
+  /* Active link styling */
   .router-link-active {
     font-weight: bold;
   }
 </style>
-  
